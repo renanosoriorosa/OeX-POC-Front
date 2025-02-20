efe34fcb0eacb5019bb328f6a6e6b446aa7f0c20
Renan Osorio <renanosoriogd@gmail.com>
2025-02-20 19:10:57 -0300

    Criado tela de Usuario

--- src/app/Usuario/page.tsx
+++ src/app/Usuario/page.tsx
@@ -1,7 +1,72 @@
-import withAuth from "@/utils/withAuth";
+"use client";
+import * as React from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 
-function Usuario() {
-  return <h1>usuários</h1>;
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID" },
+  { field: "firstName", headerName: "First name", flex: 1.5, },
+  { field: "lastName", headerName: "Last name",flex: 1,},
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    flex: 1,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    flex: 1.5,
+    valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
+  },
+  {
+    field: "actions",
+    headerName: "Ações",
+    width: 150,
+    renderCell: (params) => (
+      <Button
+        variant="contained"
+        color="primary"
+        size="small"
+        onClick={() => handleButtonClick(params.row.id)}
+      >
+        Editar
+      </Button>
+    ),
+  },
+];
+
+const handleButtonClick = (row: number) => {
+  console.log("Clicou em:", row);
+};
+
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
+
+const paginationModel = { page: 0, pageSize: 5 };
+
+export default function Usuario() {
+  return (
+    <Paper sx={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{ pagination: { paginationModel } }}
+        pageSizeOptions={[5, 10]}
+        sx={{ border: 0 }}
+      />
+    </Paper>
+  );
 }
-
-export default withAuth(Usuario);
